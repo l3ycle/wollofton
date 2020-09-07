c6d77bbab4be05b2339d2e0a75bc71e2db2ed4e3
l3ycle <github@b.fam-reichel.de>
2020-09-07 08:31:46 +0200

    phone site load

--- js/master.js
+++ js/master.js
@@ -1,12 +1,13 @@
 // Hello World
 //
-// launch in pc or phone page<
+// launch in pc or phone page
+
 // In index2.html
 // if (window.innerWidth < 960) {
-//     window.location = "https://l3ycle.github.io/wollofton/index_phone.html";
+//     window.location = "./index_phone.html";
 // }
 //
 // // In index1.html
 // if (window.innerWidth >= 960) {
-//     window.location = "https://l3ycle.github.io/wollofton/index.html";
+//     window.location = "./index_laptop.html";
 // }
