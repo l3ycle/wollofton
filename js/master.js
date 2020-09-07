86ad70b59a9ebe2a46e65126c676b179a2ab8120
l3ycle <github@b.fam-reichel.de>
2020-09-07 08:47:29 +0200

    finish laptop and phone site loading

--- js/master.js
+++ js/master.js
@@ -1,13 +1,1 @@
-// Hello World
-//
-// launch in pc or phone page
 
-// In index2.html
-// if (window.innerWidth < 960) {
-//     window.location = "./index_phone.html";
-// }
-//
-// // In index1.html
-// if (window.innerWidth >= 960) {
-//     window.location = "./index_laptop.html";
-// }
