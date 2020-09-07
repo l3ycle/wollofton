856b72143983eecfd97866534569ab15b6ac8b3b
l3ycle <github@b.fam-reichel.de>
2020-09-07 08:14:52 +0200

    add phone

--- js/master.js
+++ js/master.js
@@ -1,12 +1,12 @@
-//Hello World
-
-//launch in pc or phone page
+// Hello World
+//
+// launch in pc or phone page<
 // In index2.html
-if (window.innerWidth < 960) {
-    window.location = "https://l3ycle.github.io/wollofton/index_phone.html";
-}
-
-// In index1.html
-if (window.innerWidth >= 960) {
-    window.location = "https://l3ycle.github.io/wollofton/index.html";
-}
+// if (window.innerWidth < 960) {
+//     window.location = "https://l3ycle.github.io/wollofton/index_phone.html";
+// }
+//
+// // In index1.html
+// if (window.innerWidth >= 960) {
+//     window.location = "https://l3ycle.github.io/wollofton/index.html";
+// }
